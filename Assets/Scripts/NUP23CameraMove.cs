859b5cc331011396e0529617c19be595d7dfe3b8
Oleg Chumakov <olegchumakovzulman@gmail.com>
2024-03-20 19:21:33 +0200

    initial

---
+++ Assets/Scripts/NUP23CameraMove.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NUP23CameraMove : MonoBehaviour {
+
+    private float rSpeed = 3.0f;
+    private float mSpeed = 20.0f;
+    private float X = 0.0f;
+    private float Y = 0.0f;
+    float _distance;
+
+    public Transform Target;
+
+    void Start()
+    {
+        _distance = Vector3.Distance(Target.position,transform.position);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void Update()
+    {
+        Move(Time.deltaTime);
+    }
+
+    void Move(float dt)
+    {
+
+        // transform.position += Vector3.up*dt;
+        // https://docs.unity3d.com/ScriptReference/Vector3.Cross.html
+        // transform.position += Vector3.Cross(Vector3.up, Vector3.forward)*dt;
+        // For normalized vectors Dot returns 1 if they point in exactly the same direction, -1 if they point in completely opposite directions and zero if the vectors are perpendicular.
+        // Debug.Log(Vector3.Dot(Vector3.up, Vector3.forward));
+        // transform.position += Vector3.up*dt;
+
+        // Quaternion target = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y,transform.rotation.eulerAngles.z+dt);
+
+        // Dampen towards the target rotation
+        // transform.rotation = target;
+
+        // Vector3 direction = (Target.position - transform.position).normalized;
+        // transform.position += direction*dt;
+
+
+
+        // transform.position += Vector3.up*dt;
+
+        // transform.Rotate(0, 0, 2 * dt);
+
+        // X += Input.GetAxis("Mouse X") * rSpeed;
+        // Y += Input.GetAxis("Mouse Y") * rSpeed;
+
+
+        // transform.localRotation = Quaternion.AngleAxis(X, Vector3.up);
+        // transform.localRotation *= Quaternion.AngleAxis(Y, Vector3.left);
+
+
+        // transform.position += transform.forward * mSpeed * Input.GetAxis("Vertical")*dt;
+        // transform.position += transform.right * mSpeed * Input.GetAxis("Horizontal")*dt;
+    }
+
+        //     a.position += Vector3.up*Time.fixedDeltaTime;
+        // b.position += Vector3.up*Time.smoothDeltaTime;
+        // c.position += Vector3.up*Time.deltaTime;
+
+
+        // e.position = (a.position+b.position+c.position)/3;
+}
---
+++ Assets/Shaders/NUP23_Specular.shader
@@ -0,0 +1,47 @@
+Shader "Custom/NUP24_Specular"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _Smoothness ("Smoothness", Range(0,1)) = 0.0
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        half _Smoothness;
+        fixed4 _Color;
+        half _Metallic;
+
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+            o.Albedo = c.rgb;
+            o.Alpha = c.a;
+
+            o.Smoothness = _Smoothness;
+
+            //https://docs.unity3d.com/Manual/SL-SurfaceShaders.html
+            //o.Metallic = half;
+            //o.Emission = color;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
